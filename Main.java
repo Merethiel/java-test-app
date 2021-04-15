55e5ac28d0577c9ca52448cdd57aae1c03320d36
Jade <jade@Jades-MacBook-Pro.local>
2021-04-15 07:44:59 -0400

    initial:

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.net.*;
+public class Main {
+    public static void main(String arg[]){
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            Socket s = server.accept();
+
+            System.out.println("Connected");
+        }catch(Exception e){}
+    }
+}
